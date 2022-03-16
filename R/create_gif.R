b2f236bdedc065ec81c1eb5e1064ff1cf1d53e48
William Tyler Bradley <william.tyler.bradley@gmail.com>
2022-03-15 22:17:15 -0400

    styler

--- R/create_gif.R
+++ R/create_gif.R
@@ -16,31 +16,33 @@
 #' @export
 create_gif <- function(seeds, size, anchor_layout, hue_turn, color_scheme,
                        color_subset_center, color_subset_width,
-                       movement, output_file){
-
-  points <- get_anchor_points(seeds, size, anchor_layout,
-                              hue_turn, color_scheme,
-                              color_subset_center, color_subset_width)
+                       movement, output_file) {
+  points <- get_anchor_points(
+    seeds, size, anchor_layout,
+    hue_turn, color_scheme,
+    color_subset_center, color_subset_width
+  )
   paths <- get_paths(points, seeds)
   point_paths <- get_point_paths(points, paths)
 
-  axes_limits <- max(c(abs(c(paths$x,
-                             paths$y,
-                             paths$xend,
-                             paths$yend))))
+  axes_limits <- max(c(abs(c(
+    paths$x,
+    paths$y,
+    paths$xend,
+    paths$yend
+  ))))
 
   tp_dr <- tempdir()
 
-  if(movement == "march") {
-
+  if (movement == "march") {
     max_frame <- max(paths$time) + 9
-    for(frame in 0:max_frame) { # check max time plus something
+    for (frame in 0:max_frame) { # check max time plus something
       sub_paths <- paths %>%
         dplyr::ungroup() %>%
         dplyr::filter(.data$time > frame - 10 & .data$time <= frame) %>%
         dplyr::mutate(alpha_value = 1 - (max(.data$time) - .data$time) / 10)
 
-      if(frame <= 10) {
+      if (frame <= 10) {
         point_paths <- point_paths %>%
           dplyr::mutate(alpha_value = (10 - frame) / 10)
       } else {
@@ -48,39 +50,52 @@ create_gif <- function(seeds, size, anchor_layout, hue_turn, color_scheme,
       }
 
       ggplot2::ggplot() +
-        ggplot2::geom_point(data = point_paths,
-                            ggplot2::aes(x = .data$x, y = .data$y,
-                                         color = .data$hex_color,
-                                         alpha = .data$alpha_value),
-                            size = .25,
-                            stroke = 0, shape = 16) +
-        ggplot2::geom_segment(data = sub_paths,
-                              ggplot2::aes(x = .data$x, y = .data$y,
-                                           xend = .data$xend, yend = .data$yend,
-                                           color = .data$hex_color,
-                                           alpha = .data$alpha_value),
-                              lineend = "round",
-                              linejoin = "round",
-                              size = .15) +
+        ggplot2::geom_point(
+          data = point_paths,
+          ggplot2::aes(
+            x = .data$x, y = .data$y,
+            color = .data$hex_color,
+            alpha = .data$alpha_value
+          ),
+          size = .25,
+          stroke = 0, shape = 16
+        ) +
+        ggplot2::geom_segment(
+          data = sub_paths,
+          ggplot2::aes(
+            x = .data$x, y = .data$y,
+            xend = .data$xend, yend = .data$yend,
+            color = .data$hex_color,
+            alpha = .data$alpha_value
+          ),
+          lineend = "round",
+          linejoin = "round",
+          size = .15
+        ) +
         ggplot2::scale_color_identity() +
         ggplot2::scale_alpha_identity() +
         ggplot2::scale_x_continuous(limits = c(-axes_limits, axes_limits)) +
         ggplot2::scale_y_continuous(limits = c(-axes_limits, axes_limits)) +
         ggplot2::coord_equal() +
         ggplot2::theme_void() +
-        ggplot2::theme(plot.background = ggplot2::element_rect(color = "white",
-                                                               fill = "white"))
+        ggplot2::theme(plot.background = ggplot2::element_rect(
+          color = "white",
+          fill = "white"
+        ))
 
-      ggplot2::ggsave(filename = file.path(tp_dr,
-                                           paste0("image_", stringr::str_pad(frame, 3, pad = "0"), ".png")),
-                      widt = 2.5,
-                      height = 2.5)
+      ggplot2::ggsave(
+        filename = file.path(
+          tp_dr,
+          paste0("image_", stringr::str_pad(frame, 3, pad = "0"), ".png")
+        ),
+        widt = 2.5,
+        height = 2.5
+      )
     }
   } else {
-
     max_frame <- max(paths$time) * 2
-    for(frame in 0:max_frame) {
-      if(frame <= max_frame / 2) {
+    for (frame in 0:max_frame) {
+      if (frame <= max_frame / 2) {
         sub_paths <- paths %>%
           dplyr::filter(.data$time <= frame)
       } else {
@@ -91,41 +106,56 @@ create_gif <- function(seeds, size, anchor_layout, hue_turn, color_scheme,
       }
 
       ggplot2::ggplot() +
-        ggplot2::geom_point(data = point_paths,
-                            ggplot2::aes(x = .data$x, y = .data$y,
-                                         color = .data$hex_color),
-                            size = .25,
-                            stroke = 0, shape = 16) +
-        ggplot2::geom_segment(data = sub_paths,
-                              ggplot2::aes(x = .data$x, y = .data$y,
-                                           xend = .data$xend, yend = .data$yend,
-                                           color = .data$hex_color),
-                              lineend = "round",
-                              linejoin = "round",
-                              size = .15) +
+        ggplot2::geom_point(
+          data = point_paths,
+          ggplot2::aes(
+            x = .data$x, y = .data$y,
+            color = .data$hex_color
+          ),
+          size = .25,
+          stroke = 0, shape = 16
+        ) +
+        ggplot2::geom_segment(
+          data = sub_paths,
+          ggplot2::aes(
+            x = .data$x, y = .data$y,
+            xend = .data$xend, yend = .data$yend,
+            color = .data$hex_color
+          ),
+          lineend = "round",
+          linejoin = "round",
+          size = .15
+        ) +
         ggplot2::scale_color_identity() +
         ggplot2::scale_alpha_identity() +
         ggplot2::scale_x_continuous(limits = c(-axes_limits, axes_limits)) +
         ggplot2::scale_y_continuous(limits = c(-axes_limits, axes_limits)) +
         ggplot2::coord_equal() +
         ggplot2::theme_void() +
-        ggplot2::theme(plot.background = ggplot2::element_rect(color = "white",
-                                                               fill = "white"))
+        ggplot2::theme(plot.background = ggplot2::element_rect(
+          color = "white",
+          fill = "white"
+        ))
 
-      ggplot2::ggsave(filename = file.path(tp_dr,
-                                           paste0("image_", stringr::str_pad(frame, 3, pad = "0"), ".png")),
-                      widt = 2.5,
-                      height = 2.5)
+      ggplot2::ggsave(
+        filename = file.path(
+          tp_dr,
+          paste0("image_", stringr::str_pad(frame, 3, pad = "0"), ".png")
+        ),
+        widt = 2.5,
+        height = 2.5
+      )
     }
   }
 
   imgs <- file.path(tp_dr, list.files(tp_dr, pattern = "^image_...\\.png$"))
   gifski::gifski(imgs,
-                 delay = 1/15,
-                 gif_file = output_file,
-                 width = 750,
-                 height = 750,
-                 progress = FALSE)
+    delay = 1 / 15,
+    gif_file = output_file,
+    width = 750,
+    height = 750,
+    progress = FALSE
+  )
 
   # Clean up
   unlink(imgs)
--- R/create_png.R
+++ R/create_png.R
@@ -17,21 +17,22 @@
 #' @export
 create_png <- function(seeds, size, anchor_layout, hue_turn, color_scheme,
                        color_subset_center, color_subset_width,
-                       alpha_taper, output_file){
-
-  points <- get_anchor_points(seeds, size, anchor_layout,
-                              hue_turn, color_scheme,
-                              color_subset_center, color_subset_width)
+                       alpha_taper, output_file) {
+  points <- get_anchor_points(
+    seeds, size, anchor_layout,
+    hue_turn, color_scheme,
+    color_subset_center, color_subset_width
+  )
   paths <- get_paths(points, seeds)
   point_paths <- get_point_paths(points, paths)
 
-  if(alpha_taper == "start") {
+  if (alpha_taper == "start") {
     paths <- paths %>%
       dplyr::group_by(.data$id) %>%
       dplyr::mutate(alpha_value = 1 - (max(.data$time) - .data$time) /
-                      (max(.data$time) + 1)) %>%
+        (max(.data$time) + 1)) %>%
       dplyr::ungroup()
-  } else if(alpha_taper == "end") {
+  } else if (alpha_taper == "end") {
     paths <- paths %>%
       dplyr::group_by(.data$id) %>%
       dplyr::mutate(alpha_value = 1 - (.data$time / (max(.data$time) + 1))) %>%
@@ -40,41 +41,55 @@ create_png <- function(seeds, size, anchor_layout, hue_turn, color_scheme,
     paths <- paths %>%
       dplyr::group_by(.data$id) %>%
       dplyr::mutate(alpha_value = 1 - (abs(.data$time - stats::median(.data$time)) /
-                                         (median(.data$time) + 1))) %>%
+        (median(.data$time) + 1))) %>%
       dplyr::ungroup()
   }
 
-  axes_limits <- max(c(abs(c(paths$x,
-                             paths$y,
-                             paths$xend,
-                             paths$yend))))
+  axes_limits <- max(c(abs(c(
+    paths$x,
+    paths$y,
+    paths$xend,
+    paths$yend
+  ))))
 
   ggplot2::ggplot() +
-    ggplot2::geom_point(data = point_paths,
-                        ggplot2::aes(x = .data$x, y = .data$y,
-                                     color = .data$hex_color,
-                                     alpha = .data$alpha_value),
-                        size = .25,
-                        stroke = 0, shape = 16) +
-    ggplot2::geom_segment(data = paths,
-                          ggplot2::aes(x = .data$x, y = .data$y,
-                                       xend = .data$xend, yend = .data$yend,
-                                       color = .data$hex_color,
-                                       alpha = .data$alpha_value),
-                          lineend = "round",
-                          linejoin = "round",
-                          size = .15) +
+    ggplot2::geom_point(
+      data = point_paths,
+      ggplot2::aes(
+        x = .data$x, y = .data$y,
+        color = .data$hex_color,
+        alpha = .data$alpha_value
+      ),
+      size = .25,
+      stroke = 0, shape = 16
+    ) +
+    ggplot2::geom_segment(
+      data = paths,
+      ggplot2::aes(
+        x = .data$x, y = .data$y,
+        xend = .data$xend, yend = .data$yend,
+        color = .data$hex_color,
+        alpha = .data$alpha_value
+      ),
+      lineend = "round",
+      linejoin = "round",
+      size = .15
+    ) +
     ggplot2::scale_color_identity() +
     ggplot2::scale_alpha_identity() +
     ggplot2::scale_x_continuous(limits = c(-axes_limits, axes_limits)) +
     ggplot2::scale_y_continuous(limits = c(-axes_limits, axes_limits)) +
     ggplot2::coord_equal() +
     ggplot2::theme_void() +
-    ggplot2::theme(plot.background = ggplot2::element_rect(color = "white",
-                                                           fill = "white"))
+    ggplot2::theme(plot.background = ggplot2::element_rect(
+      color = "white",
+      fill = "white"
+    ))
 
-  ggplot2::ggsave(filename = output_file,
-                  device = "png",
-                  widt = 2.5,
-                  height = 2.5)
+  ggplot2::ggsave(
+    filename = output_file,
+    device = "png",
+    widt = 2.5,
+    height = 2.5
+  )
 }
